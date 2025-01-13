1e40c7c37f25d458a34907e38ba5b1e79068f23d
jorge <llanezarodriguezjorge@gmail.com>
2025-01-13 19:55:22 +0100

    Finished game module

--- simple/Main.java
+++ simple/Main.java
@@ -1,10 +1,8 @@
 package simple;
-
 import simple.Game;
-
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.        
+        simple.Game game = new simple.Game();
+        System.out.println("The game has started, shall you hoped so or not. \n Now you shall input your desired cell to shoot to, between a range of 0 and 7");
     }
 }
