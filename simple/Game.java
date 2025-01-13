be7a26f183968f4d81843cb462eda3db0620c4d9
jorge <llanezarodriguezjorge@gmail.com>
2025-01-13 19:48:39 +0100

    Finished initial game module

--- simple/Game.java
+++ simple/Game.java
@@ -5,6 +5,7 @@ public class Game {
     private int size = 7;
     private int shipPosition;
     private boolean[] partIsHit = new boolean[this.shipSize];
+    private boolean gameEnded = false;
 
     public Game() {
         int possibleShipPositions = this.size - 2;
@@ -20,6 +21,7 @@ public class Game {
             if (this.partIsHit[hitIndex] == false) {
                 System.out.println("Its-a-hit!!");
                 this.partIsHit[hitIndex] = true;
+                checkIfGameEnded();
             } 
             else {
                 System.out.println("You can't kill the same bird twice");
@@ -28,5 +30,13 @@ public class Game {
         else {
             System.out.println("Miss!!");
         }
+    }
+    private void checkIfGameEnded() {
+        for(int i = 0; i < shipSize; i++ ) {
+            if (this.partIsHit[i] == false) {
+                return;
+            }
+        }
+        this.gameEnded = true;
     }
 }
--- simple/Main.java
+++ simple/Main.java
@@ -1,6 +1,10 @@
 package simple;
+
+import simple.Game;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Game game = new Game();
+        game.        
     }
 }
